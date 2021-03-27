10caeee74d74e2e335cc955e90cc3ca4a26a8b02
unknown <gurgelgermano@gmail.com>
2021-03-27 00:31:08 -0300

    projeto finalizado

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const pix = document.querySelector(".chave-pix")
 
 function copiarPix(){
-  
+  pix.innerHTML = "51b81827-4ae8-4806-9853-d85891cda413"
 }
 
-pix.onclick = () => {
+pix.onclick = (evento) => {
+  evento.preventDefault()
   copiarPix();
+  setTimeout(function(){ 
+    pix.innerHTML = "Chave Pix"; 
+  }, 5000);
 }
